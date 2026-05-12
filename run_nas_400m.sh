09760f73d97f94b01e99138d506905911a5c3202
David Thrower <david.thrower@outlook.com>
2026-05-11 21:52:48 -0400

    Increase max-samples from 10000 to 50000

--- run_nas_400m.sh
+++ run_nas_400m.sh
@@ -7,7 +7,6 @@ cd /app/HelixLM
 python nas_400m_poc.py \
   --n-trials 15 \
   --epochs 1 \
-  --max-samples 10000 \
+  --max-samples 50000 \
   --output-dir ./nas_400m_results \
   --dataset-repo https://huggingface.co/datasets/david-thrower/HelixLM-small-50.0Mt-91250pt-7143it-20260427
-
