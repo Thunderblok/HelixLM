b7f2a490eab70b264db98d3f2b525987d564bdac
David Thrower <david.thrower@outlook.com>
2026-09-04 10:22:36 -0400

    Increase number of samples from 1500 to 3750

--- quick_demo_cpu_multiscale.py
+++ quick_demo_cpu_multiscale.py
@@ -19,7 +19,7 @@ from helix_lm import HelixConfig, HelixForCausalLM, HelixTokenizer, PretrainTrai
 
 EPOCHS = 10
 MAX_SEQ_LEN = 96
-NUM_SAMPLES = 1_500
+NUM_SAMPLES = 3_750
 VAL_SPLIT = 0.2
 
 EXAMPLE_PROMPTS = [
